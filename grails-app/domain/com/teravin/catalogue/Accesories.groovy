038d135238fc6dfbaafeeb3894eecffb96614174
rondheking <ronald_d84@yahoo.co.uk>
2011-06-13 13:19:49 +0700

    Accesories.groovy

---
+++ grails-app/domain/com/teravin/catalogue/Accesories.groovy
@@ -0,0 +1,66 @@
+package com.teravin.catalogue
+
+import com.teravin.catalogue.maintenance.UnitType
+
+class Accesories {
+
+    static auditable = true
+
+    String name
+	double price
+	UnitType unitType
+    String description = ""
+	String deleteFlag = "N"
+    int idx = 99
+    String createdBy
+    Date dateCreated
+    String updatedBy = ""
+    Date lastUpdated
+
+    static constraints = {
+        name(blank:false,maxSize:100)
+        description(maxSize:500)
+        idx(maxSize:3)
+        createdBy(blank:false,maxSize:50)
+        dateCreated(blank:false)
+        updatedBy(maxSize:50)
+        lastUpdated()
+    }
+
+    String toString() {
+      "$name"
+    }
+
+
+  /**
+    *
+    */
+   def beforeInsert = {
+//        createdBy = springSecurityService.principal.username
+        dateCreated = new Date()
+   }
+   def beforeUpdate ={
+//        updatedBy = springSecurityService.principal.username
+        lastUpdated = new Date()
+   }
+
+    /**
+     * Audit Logging
+     */
+    def onSave = {
+		println "new Accesories inserted"
+		// may optionally refer to newState map
+	}
+	def onDelete = {
+		println "Accesories was deleted"
+                // may optionally refer to oldState map
+	}
+	def onChange = { oldMap,newMap ->
+		println "Accesories was changed ..."
+		oldMap.each({ key, oldVal ->
+			if(oldVal != newMap[key]) {
+				println " * $key changed from $oldVal to " + newMap[key]
+			}
+		})
+	}//*/
+}
